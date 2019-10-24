d7dcd85ffe82418ab0a46b56d9398aa378314a71
Niklas <nkreutzarek@hotmail.de>
2019-10-24 19:43:19 +0200

    add delete routes

--- controllers/anchors.js
+++ controllers/anchors.js
@@ -3,9 +3,18 @@ const express = require("express");
 const Anchor = require("../models/anchors");
 const uuid4 = require("uuid4");
 
-exports.getAnchor = async (req, res, next) => {
+exports.getAllAnchor = async (req, res, next) => {
   try {
     anchors = await Anchor.find({});
+    res.status(200).json(anchors);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.getAnchor = async (req, res, next) => {
+  try {
+    anchors = await Anchor.find({id: req.params.id});
     res.status(200).json(anchors);
   } catch (err) {
     console.log(err);
@@ -22,3 +31,22 @@ exports.createAnchor = async (req, res, next) => {
     console.log(err);
   }
 };
+
+exports.deleteAllAnchors = async (req, res, next) => {
+    try {
+      await Anchor.deleteMany({});
+      res.status(200).json();
+    } catch(err) {
+      console.log(err);
+    }
+}
+
+exports.deleteAnchor = async (req, res, next) => {
+  try {
+    anchorID = req.params.id;
+    await Anchor.deleteOne({id: anchorID});
+    res.status(200).json();
+  } catch(err) {
+    console.log(err);
+  }
+}
--- routes/anchors.js
+++ routes/anchors.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const anchorController = require('../controllers/anchors');
 
-router.get('/anchors',  [anchorController.getAnchor]);
+router.get('/anchors',  [anchorController.getAllAnchor]);
+router.get('/anchors/:id',  [anchorController.getAnchor]);
 router.post('/anchors',  [anchorController.createAnchor]);
+router.delete('/anchors',  [anchorController.deleteAllAnchors]);
+router.delete('/anchors/:id',  [anchorController.deleteAnchor]);
 
 module.exports = router;
